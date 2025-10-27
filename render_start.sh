1dd0e58b30f035cdafe441fd46213b9324cd4eb1
ssnsii <sithunissanka18@gmail.com>
2025-10-27 09:42:16 +0530

    Update render_start.sh

--- render_start.sh
+++ render_start.sh
@@ -1,5 +1,1 @@
-# Ensure the Bokeh server binds to all IPs and the correct port
-bokeh serve main.py \
-  --allow-websocket-origin=$RENDER_EXTERNAL_HOSTNAME \
-  --port $PORT \
-  --address 0.0.0.0
+bokeh serve main.py --allow-websocket-origin=bokeh-graph-app.onrender.com --port $PORT
